9de3bd051e454af6fec586b16d6461312c4191bb
hafei <zhangmengcoding@gmail.com>
2018-09-09 19:08:52 +0800

    enhance allitebooks

--- allitebooks.py
+++ allitebooks.py
@@ -15,35 +15,86 @@ import os
 import urllib3
 import bs4
 
-url = 'mongodb://localhost:27017/'
+site = 'http://www.allitebooks.com/'
+count = 776
+
+#db
+dburl = 'mongodb://localhost:27017/allitebooks'
 proxy_url = 'http://127.0.0.1:2080'
 http = urllib3.ProxyManager(proxy_url=proxy_url)
-booklist =[]
-count = 768
-#r = http.request('GET','http://www.google.com')
-#status = r.status
-#data = r.data
-client = MongoClient(url)
-db = client.books
-collection = db.bookurl
 
-def savebooklist(booklist,i):
-    allitebooks = 'http://www.allitebooks.com/page/'+ str(i)
+client = MongoClient(dburl)
+db = client.get_database()
+collection = db.booksinfo
+
+def savebooklist(i):
+    allitebooks = site + 'page/'+ str(i)
     all  = http.request('GET',allitebooks)
     alldata = all.data
     soup = bs4.BeautifulSoup(alldata,'html5lib')
     books = soup.select('h2 a')
+    booksinfo = []
     for book in books:
         bookurl = book['href']
-        collection.insert_one({'url':bookurl})
+        bookinfo = getBookinfo(bookurl)
+        booksinfo.append(bookinfo)
         
-#for i in range(1,count):
-#    getbooklist(booklist,i)
+    collection.insert_many(booksinfo)
         
-        
-def getbookurl():
+'''
+help methods
+'''
+def getiteminfo(item):
+    return item.text
     
-        
+def getdownloadlink(item):
+    return item['href']   
+'''
+help methods
+'''
 
-    
-    
+
+def getBookinfo(bookurl):
+    urldetail =  bookurl
+    print('bookurl: ' + urldetail)
+    bookinfopage = http.request('GET',urldetail)
+    soup = bs4.BeautifulSoup(bookinfopage.data,'html5lib')
+
+    #book information
+    bookname = soup.select('h1')[0].text
+    print('bookname: '+ bookname)
+    try:
+        author = list(map(getiteminfo,soup.select('.book-detail dl dt')[0].next_sibling.select('a')))
+        ISBN = soup.select('.book-detail dl dt')[1].next_sibling.text
+        year = soup.select('.book-detail dl dt')[2].next_sibling.text
+        pages = soup.select('.book-detail dl dt')[3].next_sibling.text
+        language = soup.select('.book-detail dl dt')[4].next_sibling.text
+        filesize = soup.select('.book-detail dl dt')[5].next_sibling.text
+        fileformat = soup.select('.book-detail dl dt')[6].next_sibling.text
+        downloadlinks = list(map(getdownloadlink,soup.select('.download-links a')))
+        bookinfo = {
+                'bookname' : bookname,
+                'bookurl': bookurl,
+                'author': author,
+                'ISBN': ISBN,
+                'year': year,
+                'pages': pages,
+                'language': language,
+                'filesize': filesize,
+                'fileformat': fileformat,
+                'downloadlinks':downloadlinks
+                }
+    except BaseException:
+        bookinfo ={
+                'bookname' : bookname,
+                'bookurl': bookurl,
+                'except': 'html parse error'
+                }
+    return bookinfo
+
+def getAllitebooks(count):
+    for i in range(21, count):
+        savebooklist(i)
+        print('Run over page '+str(i))
+
+getAllitebooks(count)
